92fdecae4f1f2f90d0506942a8c3bb0731b615bd
Laura Pedroni <laurage_@hotmail.com>
2016-07-11 19:09:24 +0200

    seed created for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@ User.destroy_all
 
 users_attributes = [
 {
-  email: "aline@gmail.com", created_at: nil, updated_at: nil, first_name: "Aline", last_name: "Soville", picture: nil, description: "Loves cooking, reading and watching leaves falling from trees", address: "25 rue du Petit Chat, 75014 Paris", phone_number: "0785964556"
-  }
+  email: "aline@gmail.com", password: "0123456789", created_at: nil, updated_at: nil, first_name: "Aline", last_name: "Soville", picture: nil, description: "Loves cooking, reading and watching leaves falling from trees", address: "25 rue du Petit Chat, 75014 Paris", phone_number: "0785964556"
+  },
 {
-  email: "leo.Maguet@hotmail.com", created_at: nil, updated_at: nil, first_name: "Leo", last_name: "Maguet", picture: nil, description: "I travel a lot for work and would love to meet friends in places I hang out", address: "Place de la Gare, 75016 Paris", phone_number: "0745869866"
-  }
+  email: "leo.Maguet@hotmail.com", password: "aaaaaaaaa", created_at: nil, updated_at: nil, first_name: "Leo", last_name: "Maguet", picture: nil, description: "I travel a lot for work and would love to meet friends in places I hang out", address: "Place de la Gare, 75016 Paris", phone_number: "0745869866"
+  },
 {
-  email: "thebestcookie@gmail.com", created_at: nil, updated_at: nil, first_name: "John", last_name: "Ravault", picture: nil, description: "Philosopher and chips lover", address: "---", phone_number: "0788656975"
+  email: "thebestcookie@gmail.com", password: "0123456789", created_at: nil, updated_at: nil, first_name: "John", last_name: "Ravault", picture: nil, description: "Philosopher and chips lover", address: "---", phone_number: "0788656975"
   }
 ]
 
