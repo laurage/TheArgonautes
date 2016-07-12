ba9eb4cfa3c01279056ae874a554926c82113dfc
Laura Pedroni <laurage_@hotmail.com>
2016-07-12 18:00:26 +0200

    static cards home generated + correct liaisons between tables + correct seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712130122) do
+ActiveRecord::Schema.define(version: 20160712154024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160712130122) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "event_id"
   end
 
   create_table "events", force: :cascade do |t|
