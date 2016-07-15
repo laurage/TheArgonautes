51cb91d16cdb6a6761d53e2870b6da86441ce59d
Laura Pedroni <laurage_@hotmail.com>
2016-07-15 17:48:33 +0200

    event view

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,4 @@ class Event < ActiveRecord::Base
    belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
-  include AlgoliaSearch
-  algoliasearch do
-    # all attributes will be sent
-  end
 end
